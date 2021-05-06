56108c1eb921fa26b1f286e1f6d4594ae42d6b2a
Casey Johnson <johnsoncm3@gmail.com>
2021-05-06 17:51:20 -0400

    fixed postNote function in store.js

--- Develop/db/store.js
+++ Develop/db/store.js
@@ -2,15 +2,27 @@
 // and readfileasync and writefileasync
 const utils = require('util')
 const fs = require('fs') 
+
+// Universally Unique Identifiers 
 const uuidv1 = require('uuidv1')
+// everytime you see new note this gets applied^^
 console.log("uuidv1" ,uuidv1())
 
 const readFileAsync = utils.promisify(fs.readFile)
 const writeFileAsync = utils.promisify(fs.writeFile)
 
-// store note as variable in a class
+
 
 const newID = uuidv1();
+
+
+// function createdUniqueId () {
+//     [id: newID,
+//     ]
+// };
+
+
+// stores note as variable in a class
 
 class Store {
     // all functions
@@ -26,23 +38,30 @@ class Store {
         return writeFileAsync('db/db.json' , JSON.stringify())
     }
     postNote(note){
-        // if statement if there's not text or title error
-        if (text == null && title == null){
-            return ("Error")
+// read the file using the fs readfile function
+// this will produce array of notes in db
+        fs.readFile('db/db.json')
 
-        }else{
-            return writeFileAsync('db/db.json' , JSON.stringify(note))
-        }
+// push new note onto the notes array notes.push(note)
+// json.stringify(notes)
+       return notes.push(note).JSON.stringify(notes) 
+        // }else{
+            // return writeFileAsync('db/db.json' , JSON.stringify(note))
+        // }
     };
 
     deleteNote(noteId){
+
+        fs.read('db/db.json')
+
         //read all the notes from the db.json file, and remove the notes
-        //with the given id property, and then rewrite the notes to the db.json file
-        //locate the note to be deleted using uuidv, which generates a random id
-        if (newId === newId){
+        //with the given id property
+     
+        if (newId === noteId){
             deleteNote()
             
         }else{
+            //then rewrite the notes to the db.json file
             return writeFileAsync('db/db.json' , JSON.stringify(note))
         }
     }
